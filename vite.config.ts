6c53c3fe7a5fa7bf6ee4b5c7a8592e94ac294b6f
t2prawanta <ttpouli70730@gmail.com>
2025-09-21 17:41:07 +0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,29 +8,24 @@ export default defineConfig({
     react(),
     runtimeErrorOverlay(),
     ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
-    },
-  },
+    process.env.REPL_ID !== undefined? [
+ await import("@replit/vite-plugin-cartographer").then((m) => m.cartographer(),),]
+],
+
   root: path.resolve(import.meta.dirname, "client"),
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
   },
+  
   server: {
     fs: {
-      strict: true,
+        resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "client", "src"),
+      "@shared": path.resolve(import.meta.dirname, "shared"),
+      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+       strict: true,
       deny: ["**/.*"],
     },
   },
