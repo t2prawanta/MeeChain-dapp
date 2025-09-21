230bbeaf3653fe039f99a994fa0adb1795a01197
t2prawanta <ttpouli70730@gmail.com>
2025-09-21 17:59:31 +0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import("@replit/vite-plugin-cartographer").then((m) => m.cartographer(),),]
 ],
   
 export default defineConfig({
-  plugins: [
+  plugins: 
     react(),
     runtimeErrorOverlay(),
-    process.env.REPL_ID !== undefined? [
- 
+    process.env.REPL_ID !== undefined?
+}),
   server: {
-    alias: {
+    alias:
       "@": path.resolve(import.meta.dirname, "client", "src"),
       "@shared": path.resolve(import.meta.dirname, "shared"),
       "@assets": path.resolve(import.meta.dirname, "attached_assets"),
        strict: true,
       deny: ["**/.*"],
-    },
-  },
-});
+  };
